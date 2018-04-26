96c0399bfc15b54338de3dbccf8b3595fdc18633
bukela <bukela@gmail.com>
2018-04-26 16:39:15 +0200

    api start

---
+++ app/Http/Resources/Product/ProductResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources\Product;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        // return parent::toArray($request);
+        return [
+            'name' => $this->name, //odredjuje sta se prikazuje u api-ju
+            'description' => $this->detail,
+        ]
+    }
+}
