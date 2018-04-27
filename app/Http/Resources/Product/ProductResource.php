ca4ac40f05a71dc3574b4d337c8b544c2a4a1541
bukela <bukela@gmail.com>
2018-04-27 12:07:41 +0200

    transform disount and price

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -16,11 +16,16 @@ class ProductResource extends JsonResource
     {
         // return parent::toArray($request);
         return [
-            'name' => $this->name, //odredjuje sta se prikazuje u api-ju i menja key (npr price se zove my price)  (transformer)
+            'name' => strtoupper($this->name), //odredjuje sta se prikazuje u api-ju i menja key (npr price se zove my price)  (transformer)
             'description' => $this->detail,
             'my price' => $this->price,
-            'stock' => $this->stock,
-            'discount' => $this->discount
+            'stock' => $this->stock == 0 ? 'Out of stock' : $this->stock,
+            'discount' => $this->discount.' %',
+            'totalPrice' => round((1 - ($this->discount/100)) * $this->price, 2),
+            'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count()) : 'No rating',
+            'href' => [
+                'reviews' => route('reviews.index', $this->id) //review ruta, pogledati rute sa pha route:list
+            ]
         ];
     }
 }
