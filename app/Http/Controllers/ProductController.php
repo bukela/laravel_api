1cc8dc7fee9d66a01b94ba87abc7ca5ca1d4f83b
bukela <bukela@gmail.com>
2018-04-27 16:40:14 +0200

    auth

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,6 +8,9 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
+    public function __construct() {
+        $this->middleware('auth:api')->except('index','show');
+    }
     /**
      * Display a listing of the resource.
      *
